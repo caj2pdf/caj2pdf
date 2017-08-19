2b0c26982435e394f632ae314fa9956eb5397f79
Li <me@wangjinli.com>
2017-08-19 23:56:41 +0800

    Refactor Parser.

--- parser.py
+++ parser.py
@@ -1,11 +1,17 @@
 import struct
 
 
-class CAJParser(object):
+class Parser(object):
     def __init__(self, filename):
         self.filename = filename
-        self.__PAGE_NUMBER_OFFSET = 0x10
-        self.__TOC_NUMBER_OFFSET = 0x110
+        with open(filename, "rb") as caj:
+            fmt = struct.unpack("4s", caj.read(4))[0].replace(b'\x00', b'').decode("gb2312")
+        if fmt == "CAJ":
+            self.__PAGE_NUMBER_OFFSET = 0x10
+            self.__TOC_NUMBER_OFFSET = 0x110
+        elif fmt == "HN":
+            self.__PAGE_NUMBER_OFFSET = 0x90
+            self.__TOC_NUMBER_OFFSET = 0x158
 
     @property
     def page_num(self):
@@ -20,3 +26,11 @@ class CAJParser(object):
             caj.seek(self.__TOC_NUMBER_OFFSET)
             [toc_num] = struct.unpack("i", caj.read(4))
             return toc_num
+
+
+class CAJParser(Parser):
+    pass
+
+
+class HNParser(Parser):
+    pass
