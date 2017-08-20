e2b41a43a3ab6087046b07b9ec83caa10982647c
Li <me@wangjinli.com>
2017-08-20 17:59:19 +0800

    add outlines without level.

--- caj2pdf.py
+++ caj2pdf.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 
 from parser import CAJParser
+from utils import add_outlines
 
 if __name__ == "__main__":
-    caj = CAJParser("test.caj")
-    print(caj.page_num)
-    print(caj.toc_num)
-    caj.output_toc("toc.tmp")
-    caj.convert("output.pdf")
+    # caj = CAJParser("test.caj")
+    # print(caj.page_num)
+    # print(caj.toc_num)
+    # caj.output_toc("toc.tmp")
+    # caj.convert("output.pdf")
+
+    toc = [
+        {
+            "title": "Outline 1",
+            "page": 0
+        },
+        {
+            "title": "Outline 2",
+            "page": 2
+        },
+        {
+            "title": "Outline 3",
+            "page": 4
+        }
+    ]
+    add_outlines(toc, "output.pdf", "ol.pdf")
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import os
+import PyPDF2.pdf as PDF
+from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
 def fnd(f, s, start=0):
@@ -31,3 +33,56 @@ def fnd_all(f, s):
             last_addr = addr
         else:
             return results
+
+
+def makeDest(pdfw, pg):
+    d = PDF.ArrayObject()
+    d.append(pdfw.getPage(pg).indirectRef)
+    d.append(PDF.NameObject("/XYZ"))
+    d.append(PDF.NullObject())
+    d.append(PDF.NullObject())
+    d.append(PDF.NullObject())
+    return d
+
+
+def add_outlines(toc, filename, output):
+    pdf_out = PdfFileWriter()
+    pdf_in = PdfFileReader(open(filename, 'rb'))
+    for p in pdf_in.pages:
+        pdf_out.addPage(p)
+    toc_num = len(toc)
+    idoix = len(pdf_out._objects) + 1
+    idorefs = [PDF.IndirectObject(x + idoix, 0, pdf_out) for x in range(toc_num + 1)]
+    ol = PDF.DictionaryObject()
+    ol.update({
+        PDF.NameObject("/Type"): PDF.NameObject("/Outlines"),
+        PDF.NameObject("/First"): idorefs[1],
+        PDF.NameObject("/Last"): idorefs[-1],
+        PDF.NameObject("/Count"): PDF.NumberObject(toc_num)
+    })
+    olitems = []
+    for t in toc:
+        oli = PDF.DictionaryObject()
+        oli.update({
+            PDF.NameObject("/Title"): PDF.TextStringObject(t["title"]),
+            PDF.NameObject("/Parent"): idorefs[0],
+            PDF.NameObject("/Dest"): makeDest(pdf_out, t["page"])
+        })
+        olitems.append(oli)
+    for ix, olitem in enumerate(olitems[:-1]):
+        olitem.update({
+            PDF.NameObject("/Next"): idorefs[ix + 2]
+        })
+    for ix, olitem in enumerate(olitems[1:]):
+        olitem.update({
+            PDF.NameObject("/Prev"): idorefs[ix + 1]
+        })
+    pdf_out._addObject(ol)
+    for i in olitems:
+        pdf_out._addObject(i)
+    pdf_out._root_object.update({
+        PDF.NameObject("/Outlines"): idorefs[0]
+    })
+    outputFile = open(output, "wb")
+    pdf_out.write(outputFile)
+    outputFile.close()
